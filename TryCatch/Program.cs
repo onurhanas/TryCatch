e5d9f0a0f5c4cbc94f98b6b39855e6a800884034
onurhanas <113357599+onurhanas@users.noreply.github.com>
2022-10-02 12:03:17 +0300

    TryCatch

---
+++ TryCatch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz");
+            int sayi=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı: "+sayi);
+            }
+            catch (Exception ex)
+            {
+                
+               Console.WriteLine("Hata: "+ex.Message.ToString());
+
+            }
+            //finally{}
+
+        try
+        {  
+            int a=int.Parse(null);
+            
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("Boş bir değer girdiniz");
+            Console.WriteLine(ex);
+           
+        }
+        }
+    }
+}
